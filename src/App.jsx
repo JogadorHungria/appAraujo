92348bb48723956666f9906584c7aeabf8aee05a
joilson <joilsonhungrianv@gmail.com>
2023-10-04 21:12:54 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
 
   return (
     <AppStyle>
-      <h1>Você ligou araujo chegou "TESTE"</h1>
+      <h1>Você ligou araujo chegou</h1>
 
       <form onSubmit={handleSubmit(teste)}>
         <Input register={register} name="origem" text="Qual a origem ?" />
